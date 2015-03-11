696d730916d070bd6a83a0088176794a393065b9
Dmitriy Khudjakov <dmitriyh@parallels.com>
2015-03-11 17:03:04 +0300

    load from url

--- 3.py
+++ 3.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8
 from HTMLParser import HTMLParser
 import re
+import urllib2
 
 class A(HTMLParser):
 	def __init__(self):
@@ -10,8 +11,9 @@ class A(HTMLParser):
 		self.lnk=""
 		self.base=[]
 		self.body=True
+		self.script=False
 	def handle_data(self,data):
-		if self.body:
+		if self.body and not(self.script):
 			self.str+=data
 #		if self.lnk and
 			if re.search(u"\.m3u",data):
@@ -20,7 +22,9 @@ class A(HTMLParser):
 #			print self.lnk
 	def handle_starttag(self,tag,atr):
 		if tag=="script":
-			self.body=False
+			self.script=True
+		if tag=="body":
+			self.body=True
 		if tag=="meta":
 			for  i in atr:
 				if i[0]=="content":
@@ -35,7 +39,9 @@ class A(HTMLParser):
 #			print  self.lnk #atr[0][1]
 	def handle_endtag(self,tag):
 		if tag=="script":
-			self.body=True
+			self.script=False
+		if tag=="body":
+			self.body=False
 		if tag=="p":
 			self.str+="\n"
 		if tag=="a":
@@ -43,10 +49,23 @@ class A(HTMLParser):
 #			print tag+"1111"
 			pass
 
-f=open("2.html","r")
+#URL="http://hdbox.ws/news/43-ip-tv-kanaly.html"
+URL="http://vk.com/club42797884"
+z=re.split("/",URL)
+BASE=z[0]+"//"+z[2]
+#f=open("1.html","r")
+f=urllib2.urlopen(URL)
+print f.getcode()
 html=f.read()
-html=html.decode('CP1251')
+#print html
+#html=html.decode('CP1251')
+#html=html.decode('UTF8')
+#html=re.sub("["+chr(0xd0)+chr(0xbf)+chr(0xbb)+"]","",html)
+#print html
 f.close()
+f1=open("33.html","wb")
+f1.write(html)
+f1.close()
 
 a=A()
 a.feed(html)
@@ -55,3 +74,12 @@ print a.base
 
 z= re.split("/",'http://hdbox.ws/engine/download.php?id=35')
 print z[0]+"//"+z[2]
+
+for i in a.base:
+	a=i[1]
+	if re.split("/",a)[0]!="http:":
+		a=BASE+a
+	f=urllib2.urlopen(a)
+	h=f.read()
+	f.close()
+	print h
